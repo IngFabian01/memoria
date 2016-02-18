fea6cc397039f639e0693c1548df6d09e8c02542
L01509448 <L01509448@itesms-imac.local>
2016-02-17 18:14:59 -0600

    Initial Commit

---
+++ memoriaTests/memoriaTests.swift
@@ -0,0 +1,36 @@
+//
+//  memoriaTests.swift
+//  memoriaTests
+//
+//  Created by L01509448 on 2/17/16.
+//  Copyright © 2016 L01509448. All rights reserved.
+//
+
+import XCTest
+@testable import memoria
+
+class memoriaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
